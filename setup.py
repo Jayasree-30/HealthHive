421f2c69559adb09f8cd22de65c665e863a2b5af
Jayasree <jayasreejsp630@gmail.com>
2025-08-05 11:15:03 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="HeahlthHive",
+    version="0.1.0",
+    author="Jayasree",
+    author_email="jayasreejsp630@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
